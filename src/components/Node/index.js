b0d2cec96ba1488dc4eb204ae5ff4a5e2b0c5499
Marwan Zogheib <mzogheib@gmail.com>
2018-11-25 10:02:54 +1100

    Minor refactor

--- src/components/Node/index.js
+++ src/components/Node/index.js
@@ -8,9 +8,7 @@ import { isFile, isFolder, hasChildren } from '../../utils/fileUtils'
 class Node extends Component {
   constructor() {
     super()
-    this.state = {
-      open: false,
-    }
+    this.state = { open: false }
     this.toggleOpen = this.toggleOpen.bind(this)
   }
 
@@ -22,24 +20,24 @@ class Node extends Component {
     this.setState({ open: !this.state.open })
   }
 
-  renderNode(node) {
-    const canRenderChildren = this.state.open && hasChildren(node)
+  renderChildNode(node, index) {
     return (
-      <div className="node" onClick={this.toggleOpen}>
-        {isFile(node) && <NodeFile node={node} />}
-        {isFolder(node) && <NodeFolder node={node} open={this.state.open} />}
-        {canRenderChildren &&
-          node.children.map((childNode, index) => (
-            <div key={index} className="node__child">
-              <Node node={childNode} />
-            </div>
-          ))}
+      <div key={index} className="node__child">
+        <Node node={node} />
       </div>
     )
   }
 
   render() {
-    return this.renderNode(this.props.node, this.props.depth)
+    const node = this.props.node
+    const canRenderChildren = this.state.open && hasChildren(node)
+    return (
+      <div className="node" onClick={this.toggleOpen}>
+        {isFile(node) && <NodeFile node={node} />}
+        {isFolder(node) && <NodeFolder node={node} open={this.state.open} />}
+        {canRenderChildren && node.children.map(this.renderChildNode)}
+      </div>
+    )
   }
 }
 
