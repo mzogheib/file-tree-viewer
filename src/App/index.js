2e9678da7ee46d38185eec8752d7748b7e6d85da
Marwan Zogheib <mzogheib@gmail.com>
2018-11-24 16:08:45 +1100

    Restructure and add scss support

---
+++ src/App/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './style.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <span>Hello React</span>
+      </div>
+    );
+  }
+}
+
+export default App;
